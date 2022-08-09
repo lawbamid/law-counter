6023cc23be0f0dc76d603da9bb402fd65ca71150
De-law <lawbamid@gmail.com>
2022-08-09 15:34:48 +0100

    third commit

--- app.js
+++ app.js
@@ -2,17 +2,29 @@ let number = document.getElementById('num')
 let dec = document.getElementById('Dec')
 let Res = document.getElementById('Res')
 let Inc = document.getElementById('Inc')
+let body = document.body
+
+
 
 function Decrease (){
   number.innerText--
+  if(number.innerText < 0){
+    body.style.backgroundColor = 'red'
+  }
 }
 
 let Reset = function(){
      number.innerText = 0
+     if(Number(number.innerText) === 0){
+        body.style.backgroundColor = 'blue'
+     }
 }
 
 Res.addEventListener('click', Reset)
 
 Inc.addEventListener('click',()=>{
     number.innerText++
+    if(number.innerText > 0){
+        body.style.backgroundColor = 'green'
+    }
 })
